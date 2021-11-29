42084a169f070d477423e5e00ad24ace236ca6c0
Mario Rocha <mrocha51248@gmail.com>
2021-11-29 17:20:36 +0100

    q5

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,23 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/program", name="program_")
+ */
+class ProgramController extends AbstractController
+{
+    /**
+     * @Route("/{id<\d+>}", methods={"GET"}, name="show")
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
+        ]);
+    }
+}
