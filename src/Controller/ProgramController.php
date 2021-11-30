3aae335dc38074b336da93ce176af55eb171b988
Mario Rocha <mrocha51248@gmail.com>
2021-11-30 16:56:35 +0100

    q10

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Program;
+use App\Entity\Season;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -26,13 +27,13 @@ class ProgramController extends AbstractController
     }
 
     /**
-     * @Route("/{id<^\d+$>}", methods={"GET"}, name="show")
+     * @Route("/{id<^\d+$>}", name="show")
      */
     public function show(int $id): Response
     {
         $program = $this->getDoctrine()
             ->getRepository(Program::class)
-            ->findOneBy(['id' => $id]);
+            ->findOneById($id);
 
         if (!$program) {
             throw $this->createNotFoundException(
@@ -42,6 +43,37 @@ class ProgramController extends AbstractController
 
         return $this->render('program/show.html.twig', [
             'program' => $program,
+        ]);
+    }
+
+    /**
+     * @Route("/{programId<^\d+$>}/season/{seasonId<^\d+$>}", name="season_show")
+     */
+    public function showSeason(int $programId, int $seasonId): Response
+    {
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneById($programId);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : ' . $programId . ' found in program\'s table.'
+            );
+        }
+
+        $season = $this->getDoctrine()
+            ->getRepository(Season::class)
+            ->findOneBy(['program' => $program, 'id' => $seasonId]);
+
+        if (!$season) {
+            throw $this->createNotFoundException(
+                'No season with id : ' . $seasonId . ' found for program : ' . $program->getName() . '.'
+            );
+        }
+
+        return $this->render('program/season_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
         ]);
     }
 }
