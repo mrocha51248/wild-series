9eeb20fb0389398ea43c0d6f113cc329893f213e
Mario Rocha <mrocha51248@gmail.com>
2021-12-13 09:50:12 +0100

    q20 logout

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -23,4 +23,11 @@ class LoginController extends AbstractController
             'error'         => $error,
         ]);
     }
+
+    #[Route('/logout', name: 'logout')]
+    public function logout()
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
 }
